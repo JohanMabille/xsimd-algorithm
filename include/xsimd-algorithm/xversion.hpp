bd2f5a232306a6aefd7fc107cebae105008af54c
Johan Mabille <johan.mabille@gmail.com>
2022-09-26 22:59:42 +0200

    Headers and build files

---
+++ include/xsimd-algorithm/xversion.hpp
@@ -0,0 +1,20 @@
+/***************************************************************************
+ * Copyright (c) Johan Mabille, Sylvain Corlay, Wolf Vollprecht and         *
+ * Martin Renou                                                             *
+ * Copyright (c) QuantStack                                                 *
+ * Copyright (c) Serge Guelton                                              *
+ *                                                                          *
+ * Distributed under the terms of the BSD 3-Clause License.                 *
+ *                                                                          *
+ * The full license is in the file LICENSE, distributed with this software. *
+ ****************************************************************************/
+
+#ifndef XSIMD_ALGORITHM_VERSION_HPP
+#define XSIMD_ALGORITHM_VERSION_HPP
+
+#define XSIMD_ALGORITHM_VERSION_MAJOR 0
+#define XSIMD_ALGORITHM_VERSION_MINOR 1
+#define XSIMD_ALGORITHM_VERSION_PATCH 0
+
+#endif
+
